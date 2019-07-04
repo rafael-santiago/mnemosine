07ab1d1e6dc80d2d463d13789e180db39d3853ef
rafael-santiago <voidbrainvoid@tutanota.com>
2019-07-04 14:38:01 -0800

    Test's infrastructure

---
+++ src/test/main.c
@@ -0,0 +1,15 @@
+/*
+ *                          Copyright (C) 2019 by Rafael Santiago
+ *
+ * Use of this source code is governed by GPL-v2 license that can
+ * be found in the COPYING file.
+ *
+ */
+#include <cutest.h>
+
+CUTE_DECLARE_TEST_CASE(mnemosine_tests);
+
+CUTE_TEST_CASE(mnemosine_tests)
+CUTE_TEST_CASE_END
+
+CUTE_MAIN(mnemosine_tests);
